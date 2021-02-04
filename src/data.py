a8f6e79fa4a86ad8c5c720ee2920b6672d0048a0
bakeryproducts <gleb.m.sokolov@gmail.com>
2021-02-04 12:47:08 -0500

    path fixes, naming

--- src/data.py
+++ src/data.py
@@ -1,33 +1,30 @@
-import numpy as np
 import json
-from shapely.geometry import Polygon
 from typing import List, Tuple
-# from osgeo import gdal
+
+import numpy as np
+from shapely.geometry import Polygon
 import rasterio
 from rasterio.windows import Window
 
-from src.utilis import polyg_to_mask
+from utils import polyg_to_mask
 
 
-class Sampler(object):
+class GdalSampler:
     """Sample tiff file/img and return sampled_img and mask where 1 - glomerulus and 0 - no glomerulus.
     """
 
     def __init__(self, img_path: str, img_markup_path: str, wh: Tuple[int], mask_wh: Tuple[int]):
         with open(img_markup_path) as json_file:
             self._markups = json.load(json_file)
-        # self._img = gdal.Open(img_path, gdal.GA_ReadOnly)
         self._img = rasterio.open(img_path)
         self._wh = wh
         self._mask_wh = mask_wh
+        self._bands = (1,2,3)
+        self._count = -1
 
-        self.count = -1
-
-    def read_img(self, key: int):
-        """ Read/slice image.
-        """
+    def read_part(self, idx: int):
         # List of coordinates
-        polyg = self._markups[key]['geometry']['coordinates'][0]
+        polyg = self._markups[idx]['geometry']['coordinates'][0]
         # (x_cent,y_cent)
         polyg_cent_coord = np.round(Polygon(polyg).centroid)
         # Polygon with center in (0, 0)
@@ -35,7 +32,7 @@ class Sampler(object):
         # Set window params
         xoff, yoff = int(polyg_cent_coord[0] - self._wh[0] // 2), int(polyg_cent_coord[1] - self._wh[1] // 2)
         xsize, ysize = self._wh[0], self._wh[1]
-        return self._img.read((1, 2, 3), window=Window(xoff, yoff, xsize, ysize)), \
+        return self._img.read(self._bands, window=Window(xoff, yoff, xsize, ysize)), \
                polyg_to_mask(polyg_norm, self._mask_wh)
 
     def __iter__(self):
@@ -52,8 +49,5 @@ class Sampler(object):
     def __len__(self):
         return len(self._markups)
 
-    def __getitem__(self, key: int) -> Tuple[np.ndarray, np.ndarray]:
-        if key < len(self._markups):
-            return self.read_img(key)
-        else:
-            raise IndexError
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
+        self.read_img(key)

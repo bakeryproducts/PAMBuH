f302f82928626e142d8467fd5f03fdce9276deed
Demo <andrey.kurzyukov@fau.de>
2021-02-06 16:36:54 +0100

    Fix: - Sample mask fro tiff

--- src/sampler.py
+++ src/sampler.py
@@ -6,29 +6,34 @@ from shapely.geometry import Polygon
 import rasterio
 from rasterio.windows import Window
 
-from utils import polyg_to_mask
+from utils import jread
 
 
 class GdalSampler:
     """Iterates over img with annotation, returns tuples of img, mask
     """
 
-    def __init__(self, img_path: str, img_markup_path: str, wh: Tuple[int], mask_wh: Tuple[int]):
-        with open(img_markup_path) as json_file:
-            self._markups = json.load(json_file)
+    def __init__(self, img_path: str, mask_path: str, img_polygs_path: str, wh: Tuple[int], mask_wh: Tuple[int]):
+        self._markups = jread(img_polygs_path)
+        self._masks = rasterio.open(mask_path)
         self._img = rasterio.open(img_path)
+
         self._wh = wh
         self._mask_wh = mask_wh
-        self._bands = (1, 2, 3)
+        self._bands_img = (1, 2, 3)
+        self._bands_mask = 1
         self._count = -1
-        self._fill_value = 255
-
-        self.polygs = [markup['geometry']['coordinates'][0] for markup in self._markups]
-        self.polygs_norm = [(np.array(polyg) - np.array(Polygon(polyg).centroid) + np.array([mask_wh[0] // 2, mask_wh[0] // 2])).
+        # Get polygons
+        self.polygs = [polyg['geometry']['coordinates'][0] for polyg in self._markups]
+        self.polygs_norm = [(np.array(polyg) - np.array(Polygon(polyg).centroid) + np.array([mask_wh[0] // 2,
+                                                                                             mask_wh[0] // 2])).
                                 tolist() for polyg in self.polygs]
         polygs_cent_coord = [np.round(Polygon(polyg).centroid) for polyg in self.polygs]
-        self.xoff = [int(polyg_cent_coord[0] - self._wh[0] // 2) for polyg_cent_coord in polygs_cent_coord]
-        self.yoff = [int(polyg_cent_coord[1] - self._wh[1] // 2) for polyg_cent_coord in polygs_cent_coord]
+        # Get offsets
+        self.xoff_img = [int(polyg_cent_coord[0] - self._wh[0] // 2) for polyg_cent_coord in polygs_cent_coord]
+        self.yoff_img = [int(polyg_cent_coord[1] - self._wh[1] // 2) for polyg_cent_coord in polygs_cent_coord]
+        self.xoff_mask = [int(polyg_cent_coord[0] - self._mask_wh[0] // 2) for polyg_cent_coord in polygs_cent_coord]
+        self.yoff_mask = [int(polyg_cent_coord[1] - self._mask_wh[1] // 2) for polyg_cent_coord in polygs_cent_coord]
 
     def __iter__(self):
         return self
@@ -45,7 +50,8 @@ class GdalSampler:
             raise StopIteration("Failed to proceed to the next step")
 
     def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
-        window = Window(self.xoff[idx], self.yoff[idx], *self._wh)
-        img = self._img.read(self._bands, window=window)
-        mask = polyg_to_mask(self.polygs_norm[idx], self._mask_wh, self._fill_value)
+        window_img = Window(self.xoff_img[idx], self.yoff_img[idx], *self._wh)
+        window_mask = Window(self.xoff_mask[idx], self.yoff_mask[idx], *self._mask_wh)
+        img = self._img.read(self._bands_img, window=window_img)
+        mask = self._masks.read(self._bands_mask, window=window_mask)
         return img, mask

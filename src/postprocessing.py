9c7d8439d9a9424e32a8401c800bb1134259301a
alex <alexgalayda@gmail.com>
2021-02-11 00:09:34 +0300

    added postproc concat

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,7 +1,73 @@
-import torch
 import numpy as np
+import torch
+import torch.nn.functional as F
 
 from typing import NoReturn, Union
+
+def read_frame(fd, x, y, h, w=None, c=3):
+	if w is None: w = h
+	img = fd.read(list(range(1, c+1)), window=Window(x, y, h, w))
+	return torch.ByteTensor(img)
+
+def block_reader(path, inf, size=512, infer_list_flg=False):
+	fd, shape, channel = get_basics_rasterio(path)
+	
+	rows = []
+	for ny in tqdm(range(-size//4, shape[0], size//2), desc='rows'):
+		pad=(size//4, size - shape[1]%size)
+		if ny < 0:
+			pad = (size//4, size - shape[1]%size, size//4, 0)
+		elif shape[0]-ny < size:
+			pad = (size//4, size - shape[1]%size, 0, size + ny - shape[0])
+		row = read_frame(fd, 0, ny, shape[1], size)
+		row_img = F.pad(row, pad=pad, mode='constant', value=0)
+		row_img = row_img.unsqueeze(0)
+		left_i = torch.split(row_img, size, dim=3)[:-1]
+		right_i = torch.split(row_img[:, :, :, size//4:], size, dim=3)
+
+		imgs_batch = []
+		for i in range(len(left_i)):
+			imgs_batch.extend([left_i[i], right_i[i]])
+		if infer_list_flg:
+			infer_batch = inf(imgs_batch)
+		else:
+			infer_batch = inf(torch.cat(imgs_batch, 0))
+		infer_batch = infer_batch[:, :, size//4:-size//4, size//4:-size//4]
+		rows.append(torch.cat([i for i in infer_batch], 2).squeeze(0))
+	return torch.cat(rows, 0)[:shape[0], :shape[1]]
+
+def prt(img):
+	print(img.shape)
+	if len(img.shape) == 2:
+		plt.imshow(np.array(img))
+	else:
+		plt.imshow(np.array(img).transpose(1, 2, 0))
+	plt.show()
+
+def save_tiff_uint8_single_band(img, path):
+	if isinstance(img, torch.Tensor):
+		img = np.array(img)
+	elif not isinstance(img, numpy.ndarray):
+		raise TypeError(f'Want torch.Tensor or numpy.ndarray, but got {type(img)}')
+	assert img.dtype == np.uint8
+	h, w = img.shape
+	dst = rasterio.open(path, 'w', driver='GTiff', height=h, width=w, count=1, nbits=1, dtype=np.uint8)
+	dst.write(img, 1) # 1 band
+	dst.close()
+	print(f'Save to {path}')
+	del dst
+
+def postprocess(infer_func, src_folder, dst_folder, save_predicts=True):
+	"""
+	infer_func([BxCxHxV]) -> [BxCxHxV]
+	src_folder - folder with test images
+	dst_folder - folder to save output (RLE and predictions)
+	"""
+	imgs_name = Path(src_folder).glob('*.tiff')
+	create_dir(dst_folder)
+	for img_name in tqdm(imgs_name, desc='Images', leave=False):
+		img = block_reader(img_name, infer_func)
+		save_tiff_uint8_single_band(img, Path(dst_folder) / img_name.name)
 
 class SmoothTiles:
 	def __init__(self, window_fun: str ='triangle', cuda: bool = True) -> NoReturn:

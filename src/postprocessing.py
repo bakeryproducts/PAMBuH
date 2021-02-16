8ff05ae65853bd66de829c6b36cfcfda9b778f9b
alex <alexgalayda@gmail.com>
2021-02-16 05:02:38 +0300

    added timestemp

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,12 +1,12 @@
-#from tqdm import tqdm
 import csv
+#from tqdm import tqdm
 from tqdm.notebook import tqdm
 import numpy as np
 import torch
 import torch.nn.functional as F
 from pathlib import Path
 from typing import NoReturn, Union
-
+from datetime import datetime
 import torch
 import numpy as np
 from tqdm.auto import tqdm
@@ -22,22 +22,22 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def read_and_process_img(path: str, do_infer, block_size: int = 2048, crop_size: int = 2000, batch_size: int = 4, device : str = 'cuda') -> np.ndarray:
-	""" Giga Function doing everything. made by galayda 
+def read_and_process_img(path: str, do_infer, block_size: int = 2048, crop_size: int = 2000, batch_size: int = 4, device : str = 'cuda') -> torch.tensor:
+	""" Make prediction mask of big tiff by little crop
 
 	Args:
-		path (str): [description]
-		do_infer ([type]): [description]
-		block_size (int, optional): [description]. Defaults to 2048.
-		crop_size (int, optional): [description]. Defaults to 2000.
-		batch_size (int, optional): [description]. Defaults to 4.
+		path (str): [Path to image]
+		do_infer (function: [list of tensor [CxHxW] -> tensor [Bx1xHxW]]): infer function
+		block_size (int): [size of block for infer function]. Defaults to 2048.
+		crop_size (int): [size of crop, that will be connected in filal mask]. Defaults to 2000.
+		batch_size (int): [size of batch for infer function]. Defaults to 4.
 
 	Returns:
-		np.ndarray: [description]
+		torch.tensor: [infer mask]: [HxW]
 	"""    
 	assert block_size >= crop_size, (block_size, crop_size)
 	fd, (h, w), channel = get_basics_rasterio(path)
-	print(h, w)
+	print(f'input shape (h, w): {h, w}')
 	pad = (block_size - crop_size)//2
 	rows = []
 	for y in tqdm(range(-pad, h, crop_size), desc='rows'):
@@ -47,29 +47,38 @@ def read_and_process_img(path: str, do_infer, block_size: int = 2048, crop_size:
 			pad_y = (pad if y < 0 else 0, y + block_size - h if y + block_size > h else 0)
 			pad_chw = ((0, 0), pad_y, pad_x)
 			block = get_tiff_block(fd, x, y, block_size)
-			pad_block = np.pad(block, pad_chw, 'constant', constant_values=0)
-			if pad_block.max() > 0:
-				row.append(pad_block)
+			block = np.pad(block, pad_chw, 'constant', constant_values=0)
+			if block.max() > 0:
+				row.append(block)
 			else:
 				zeros_idx.append(i)
 		if row:
-			nozero_masks = []
+			masks = []
 			for chunk in chunks(row, batch_size):
-				nozero_masks.append(do_infer(chunk)[:, :, pad:-pad, pad:-pad])
-			nozero_masks = torch.cat(nozero_masks, 0)
-			device = nozero_masks.device
-			masks_list = [i.squeeze(0) for i in nozero_masks]
+				masks.append(do_infer(chunk)[:, :, pad:-pad, pad:-pad])
+			masks = torch.cat(masks, 0)
+			device = masks.device
+			masks_list = [i.squeeze(0) for i in masks]
 			for i in zeros_idx:
 				masks_list.insert(i, torch.zeros((crop_size, crop_size)).to(device))
-			mask_row = torch.cat(masks_list, 1)[:, :w]
+			masks = torch.cat(masks_list, 1)[:, :w]
 		else:
-			mask_row = torch.zeros((crop_size, w)).to(device)
-		rows.append(mask_row)
+			masks = torch.zeros((crop_size, w)).to(device)
+		rows.append(masks)
 	mask = torch.cat(rows, 0)[:h, :w]
 	assert mask.shape == (h, w)
 	return mask
 
-def float_mask2_01uint8(mask, threshold=0.5):
+def float_mask2_01uint8(mask : torch.tensor, threshold : float=0.5):
+	""" Convert float mask (0..1) to binary mask [0, 1]
+
+	Args:
+		mask (torch.tensor): [Mask, any element of that beetween 0 and 1].
+		threshold (float, 0..1). Defaults to 0.5.
+
+	Returns:
+		torch.tensor: infer mask (int8)
+	"""    
 	return torch.as_tensor(mask > threshold, dtype=torch.int8)
 
 def _plot_img(img: np.ndarray) -> NoReturn:
@@ -87,7 +96,7 @@ def postprocess_test_folder(infer_func, src_folder : str, dst_folder : str) -> N
 	dst_folder - folder to save output (RLE and predictions)
 	"""
 	imgs_name = Path(src_folder).glob('*.tiff')
-	dst_folder = Path(dst_folder)
+	dst_folder = Path(dst_folder) / str(datetime.now()).replace(' ', '_')
 	dst_folder.mkdir(parents=True, exist_ok=True)
 	with open(dst_folder / 'sample_submission.csv', 'w', newline='') as csvfile:
 		spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
@@ -95,6 +104,6 @@ def postprocess_test_folder(infer_func, src_folder : str, dst_folder : str) -> N
 		for img_name in tqdm(imgs_name, desc='Test images', leave=False):
 			mask = read_and_process_img(img_name, infer_func)
 			mask01 = np.uint8(float_mask2_01uint8(mask).to('cpu'))
-			#save_tiff_uint8_single_band(mask01, dst_folder / img_name.name)
-			spamwriter.writerow([img_name.stem, mask2rle(mask01.T)])
+			save_tiff_uint8_single_band(mask01, dst_folder / img_name.name)
+			spamwriter.writerow([img_name.stem, mask2rle(mask01)])
 	print(f'save to {dst_folder / "sample_submission.csv"}')
